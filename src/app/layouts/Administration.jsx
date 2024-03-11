4c66869ed64b29ae6231592a58a52202d27cff1e
pavel <kulikovps2004@gmail.com>
2024-03-11 21:49:36 +0300

    added images

--- src/app/layouts/Administration.jsx
+++ src/app/layouts/Administration.jsx
@@ -4,12 +4,15 @@ import './Administration.css';
 export const Administration = () => {
     return (
         <div className="administration">
-            <p className="heading">Администрация Муниципального образования - Малинищинское сельское поселение Пронского муниципального района Рязанской области</p>
-            <p><span className="label">Руководитель:</span><span className="value">Глава администрации Клинкова Елена Николаевна</span></p>
-            <p><span className="label">ИНН / КПП:</span><span className="value">6211005898 / 621101001</span></p>
-            <p><span className="label">Количество учредителей:</span><span className="value">1</span></p>
-            <p><span className="label">Дата регистрации:</span><span className="value">28.12.2005</span></p>
-            <p><span className="label">Статус:</span><span className="value">Действующее</span></p>
+            <img src='https://i.ibb.co/8xdCn5G/IMG-20210419-103742.jpg'/>
+            <div className='right'>
+                <p className="heading">Администрация Муниципального образования - Малинищинское сельское поселение Пронского муниципального района Рязанской области</p>
+                <p><span className="label">Руководитель:</span><span className="value">Глава администрации Клинкова Елена Николаевна</span></p>
+                <p><span className="label">ИНН / КПП:</span><span className="value">6211005898 / 621101001</span></p>
+                <p><span className="label">Количество учредителей:</span><span className="value">1</span></p>
+                <p><span className="label">Дата регистрации:</span><span className="value">28.12.2005</span></p>
+                <p><span className="label">Статус:</span><span className="value">Действующее</span></p>
+            </div>
         </div>
     );
 };
